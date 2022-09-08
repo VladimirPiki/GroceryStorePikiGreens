1bc5eec7716fcfe56954f1683c7e155f688ae06c
VladimirPiki <vlade.piki23@hotmail.com>
2022-09-08 22:22:35 +0200

    Last fot today

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 var app = angular.module('myApp', ["ngRoute"]);
 app.controller('myCtrl', function($scope) {
 	$scope.error_alert=false;
-  var fullname="";
-  $scope.firstName = "Vladimir";
-  $scope.surname = "Krstevski";
-  fullname=$scope.firstName+" "+$scope.surname;
   console.log("Ova e piljara");
 });
